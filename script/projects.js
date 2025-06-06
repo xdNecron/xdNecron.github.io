11335f1baadd218dd02b978d892e1211027990c4
xdNecron <venomdushii@gmail.com>
2025-06-06 16:45:48 +0200

    Projects functional

--- script/projects.js
+++ script/projects.js
@@ -1,52 +1,71 @@
 const textDiv = document.getElementById("text");
 
 const repos = [
-  "vscode-jumper"
+  "vscode-jumper",
+  "dotfiles"
 ];
-
-let versionTag;
-
-async function getLatestVersion(data) {
-  const res = await fetch(`${data.tags_url}`)
-    .then(json => json.json())
-    .then(data => {
-      console.log(data[0].name);
-      return data;
-    });
-  return res;
-}
 
 function renderContent(data) {
   // create a span for each repo
-  const repoContent = textDiv.appendChild(document.createElement("span"));
-  repoContent.className = "repo-entry"
+  const repoEntry = textDiv.appendChild(document.createElement("div"));
+  repoEntry.className = "repo-entry"
 
   // render repo name as heading and put it inside <a>
-  const repoLink = repoContent.appendChild(document.createElement("a"));
-  repoLink.setAttribute("href", data.html_url);
+  // heading format: <repo-name> <version>
+  const repoHeadingSpan = repoEntry.appendChild(document.createElement("span"));
+  repoHeadingSpan.className = "heading-span";
+
+  const repoLink = repoHeadingSpan.appendChild(document.createElement("a"));
+  repoLink.setAttribute("href", data.url);
   repoLink.setAttribute("target", "_blank")
 
-  const repoName = repoLink.appendChild(document.createElement("h2"));
-  repoName.innerText = data.name;
+  const repoHeading = repoLink.appendChild(document.createElement("h2"));
+  repoHeading.innerText = data.name;
+
+  console.log(data.tagName);
+
+  if (data.tagName) {
+    const repoVersionTag = repoHeadingSpan.appendChild(document.createElement("p"));
+    repoVersionTag.className = "version-tag"
+    repoVersionTag.innerText = data.tagName;
+  }
+
+  const repoContent = repoEntry.appendChild(document.createElement("span"));
+  repoContent.className = "repo-content";
 
   // render repo description
-  const desc = repoContent.appendChild(document.createElement("p"));
-  desc.innerText = data.description;
+  const repoTextSpan = repoContent.appendChild(document.createElement("span"));
+  repoTextSpan.className = "text-span";
 
-  const versionTag = repoContent.appendChild(document.createElement("small"));
-  versionTag.innerText = data.tagName;
+  const repoDesc = repoContent.appendChild(document.createElement("p"));
+  repoDesc.innerText = data.description;
 
   // add latest version, latest commit hash, message, and date
-  const commitHash = repoContent.appendChild(document.createElement("small"));
-  commitHash.innerText = data.commitHash.slice(0, 6);
+  const commitInfoSpan = repoContent.appendChild(document.createElement("span"));
+  commitInfoSpan.className = "commit-span";
 
-  const commitMessage = repoContent.appendChild(document.createElement("small"));
-  commitMessage.innerText = data.commitMessage;
+  const commitHash = commitInfoSpan.appendChild(document.createElement("small"));
+  commitHash.innerText = data.commitHash.slice(0, 6) + ":";
 
-  const updateDateText = new Date(data.commitDate);
-  console.log(updateDateText)
+  const commitMessageContent = data.commitMessage;
+
+  const updateDate = new Date(data.commitDate);
+  const updateDateOptions = {
+    era: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  const updateDateContent = updateDate.toLocaleString("us-US", updateDateOptions);
+
+  const commitMessageDate = commitInfoSpan.appendChild(document.createElement("small"));
+  commitMessageDate.innerHTML = `<i>${commitMessageContent}</i> on ${updateDateContent}.`;
 
 };
+
+// This way of fetching makes n requests to GH API where n is the length of repos[]
+// TODO rewrite the function to fetch data from api.gh.com/repos endpoint
+// That would always make only one request and then render data accordingly to repos[]
 
 for (const repo of repos.values()) {
   const repoUrl = `https://api.github.com/repos/xdNecron/${repo}`
@@ -70,18 +89,20 @@ for (const repo of repos.values()) {
       });
 
     const repoData = {
+      "url": repoInfo.html_url,
       "name": repoInfo.name,
       "description": repoInfo.description,
-      "tagName": versionTag[0].name,
       "commitHash": commitInfo[0].sha,
       "commitMessage": commitInfo[0].commit.message,
       "commitDate": commitInfo[0].commit.author.date
     };
 
-    console.log(repoData);
+    if (versionTag.length != 0) {
+      console.log("jsem definovany")
+      repoData["tagName"] = versionTag[0].name;
+    }
 
     renderContent(repoData);
   };
   getData();
-
 };
